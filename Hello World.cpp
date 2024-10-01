0263319b60360a0bbca459e98d4ef1513fae2956
deoreshraddha <shraddha.deore24@sanjivani.edu.in>
2024-10-01 23:46:51 +0530

    Create Hello World.cpp
    This simple program introduces basic syntax and structure in C++, including the use of cout for displaying output and the importance of the main() function.

---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+//Main() function: where the excution
+//Drogram begins
+int main()
+{
+    //Print hello world
+    cout<<"Hello world";
+    return 0;
+}
