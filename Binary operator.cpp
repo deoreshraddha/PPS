b94f8b646dc2abac3bdb1882acf8a5e4de2a5ef6
deoreshraddha <shraddha.deore24@sanjivani.edu.in>
2024-10-02 22:15:28 +0530

    Create Binary operator.cpp
    This C++ program demonstrates the basic arithmetic operators on two integers a and b, where a is 7 and b is 4. It performs and displays the result of five arithmetic operations:

---
+++ Binary operator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int a = 7, b = 4;
+    
+    //Addition operator 
+    cout<<"a+b="<<(a+b)<<endl;
+    
+    //Subtraction operator 
+    cout<<"a-b="<<(a-b)<<endl;
+    
+    //Multiplication operator 
+    cout<<"a*b="<<(a*b)<<endl;
+    
+    //Division operator 
+    cout<<"a/b="<<(a/b)<<endl;
+    
+    //Modulo operator 
+    cout<<"a%b="<<(a%b)<<endl;
+    
+    return 0;
+}
+    
